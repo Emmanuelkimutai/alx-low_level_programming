ee49dc5d60ce3a3c11b595290c892c874b61e750
Emmanuelkimutai <emmanuelkimtai64@gmail.com>
2022-06-17 00:52:39 +0300

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
 /**
  * main - header
  *
  */
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_stncpy(char *dest, char *src, int n);
