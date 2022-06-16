c0c2bc6afd9f00536827f3ed1739999271456378
Emmanuelkimutai <emmanuelkimtai64@gmail.com>
2022-06-17 00:56:00 +0300

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *s)
 
 	while (*(s + i))
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'Z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
 			if (i == 0)
 				*(s + i) -= 'a' - 'A';
