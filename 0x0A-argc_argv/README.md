e1bf7856140422316a3a16ca7acd4f8cb3c4f9ff
Emmanuelkimutai <emmanuelkimtai64@gmail.com>
2022-06-24 21:14:26 +0300

    program that prints the number of arguments passed into it

--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 A program that prints its name
+A program that prints the number of arguments passed into it
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,3 +1,4 @@
 #include <stdio.h>
 
 int main(int argc __attribute__((unused)), char* argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
