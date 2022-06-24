fedb94a0f541489374013fb0c5c9cf0258687d3d
Emmanuelkimutai <emmanuelkimtai64@gmail.com>
2022-06-24 21:04:31 +0300

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
  * main - print the name of the program
  *
  * @argc: The number argument in the program
- * @argv[]: The values of the arguments
+ * @argv: The values of the arguments
  *
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
