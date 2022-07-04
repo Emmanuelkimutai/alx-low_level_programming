d9b83c84ee868cf9adf4040ecf2316486e41ac70
Emmanuelkimutai <emmanuelkimtai64@gmail.com>
2022-07-04 14:54:04 +0300

    update on struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+ * struct dog - contains variables
+ *
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ */
+struct dog
 {
 	char *name;
 	float age;
